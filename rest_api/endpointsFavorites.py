e2ea99049bd8387d3766baeb7f7620a5e59ec2d5
AdrianRocaS <arocas24@fpcoruna.afundacion.org>
2026-01-27 10:30:17 +0100

    #9 Más comentarios
    Añadidos más comentarios para mayor entendimiento del código

--- rest_api/endpointsFavorites.py
+++ rest_api/endpointsFavorites.py
@@ -6,19 +6,25 @@ from .helpers import get_user_id_from_token
 from .models import Recipe, User, Session, UserFavoriteRecipes
 
 
+# Desactiva la protección CSRF porque este endpoint será consumido por una app móvil
 @csrf_exempt
 def toggle_favorite(request, recipe_id):
 
+    # Comprueba que el metodo HTTP sea POST
     if request.method != "POST":
         return JsonResponse({"error": "Method not allowed"}, status=405)
 
+    # Obtiene el ID del usuario a partir del token de autenticación
     user_id = get_user_id_from_token(request)
+
+    # Si el token no es válido o no existe, devuelve error 401 (no autorizado)
     if user_id is None:
         return JsonResponse({"error": "Invalid token"}, status=401)
 
-    # Leer JSON del body
+    # Leer JSON del body (?favorite=true o ?favorite=false)
     favorite = request.GET.get('favorite')
 
+    # Si no se ha enviado el parámetro, devuelve error 400 (petición incorrecta)
     if favorite is None:
         return JsonResponse({"error": "Missing isFavorite"}, status=400)
 
@@ -34,7 +40,7 @@ def toggle_favorite(request, recipe_id):
     else:
         UserFavoriteRecipes.objects.filter(user_id=user_id, recipe_id=recipe_id).delete()
 
-    # Respuesta OK
+    # Devuelve una respuesta JSON confirmando que funciona
     return JsonResponse({
         "message": "Favorite updated",
         "recipeId": recipe_id,
