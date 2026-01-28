93bfe66ae7aecd9e3ef49139e291bed5a8d9097e
abik <nikita161787@gmail.com>
2026-01-28 21:24:30 +0100

    bug fixes & refactoring

--- rest_api/endpointsCategory.py
+++ rest_api/endpointsCategory.py
@@ -12,15 +12,8 @@ def get_categories(request): #Define una función que Django usará como endpoin
     data = [] #Lista vacía de Python donde vamos a construir la respuesta JSON
 
     for cat in categories: #Recorre cada categoría de la base de datos
-        image_base64 = None #Por defecto indicamos que la categoría no tiene imagen
-        if cat.image: #Si tiene imagen:
-            image_base64 = base64.b64encode(cat.image).decode("utf-8")
-            #cat.image --> son bytes (binario)
-            #base64.b64encode --> convierte esos bytes en Base64
-            #decode("utf-8") --> convierte eso en texto normal (string)
-            #Como resultado obtenemos una cadena de texto que se puede enviar en JSON
-
-        data.append({ #Añadimos un nuevo elemento a la lista "data"
+    
+        data.append({
             "id": cat.id, #Id de la categoría
             "name": cat.name, #Nombre de la categoría
         })
@@ -34,14 +27,11 @@ def get_category_image(request, category_id):
     try:
         category = Category.objects.get(id=category_id)
     except Category.DoesNotExist:
-        # Si no existe, devuelve un error 404 en formato JSON
         return JsonResponse({"error": "Not found"}, status=404)
 
     # Comprueba si la categoría tiene imagen
     if not category.image:
-        # Si no tiene imagen, devuelve otro error 404
         return JsonResponse({"error": "No image"}, status=404)
-
 
     # Devuelve la imagen como respuesta HTTP
     return HttpResponse(category.image, content_type='image/jpeg')
